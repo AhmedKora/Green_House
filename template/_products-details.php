35cedc024e9c9c5dbc083d9cdcc0cbb59faab14d
AhmedKora <71018804+AhmedKora@users.noreply.github.com>
2022-06-27 22:32:24 +0200

    Add files via upload

---
+++ template/_products-details.php
@@ -0,0 +1,125 @@
+<!--   product  -->
+<?php
+    $item_id = $_GET['item_id'] ?? 1;
+    foreach ($product->getData() as $item) :
+    if ($item['item_id'] == $item_id) :
+
+        // request method post
+        if($_SERVER['REQUEST_METHOD'] == "POST"){
+            if (isset($_POST['product_details'])){
+                // call method addToCart
+                $Cart->addToCart($_POST['user_id'], $_POST['item_id']);
+            }
+        }
+?>
+
+
+<section id="product" class="py-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4"><br><br>
+                <img src="<?php echo $item['item_image'] ?>" style="height: 300px;" alt="product" class="img-fluid"><br><br>
+                <div class="form-row pt-4 font-size-16 font-baloo">
+                    <div class="col">
+                        <button type="submit" class="btn btn-danger form-control">Proceed to Buy</button>
+                    </div>
+                    <div class="col">
+                        <form method="post">
+                            <input type="hidden" name="item_id" value="<?php echo $item['item_id'] ?? '1'; ?>">
+                            <input type="hidden" name="user_id" value="<?php echo $item['user_id'] ?? '1'; ?>">
+                            <?php
+                            if (in_array($item['item_id'], $Cart->getCartId($product->getData('cart')) ?? [])){
+                                echo '<button type="submit" disabled class="btn btn-success font-size-16 form-control">In the Cart</button>';
+                            }else{
+                                echo '<button type="submit" name="product_details" class="btn btn-warning font-size-16 form-control">Add to Cart</button>';
+                            }
+                            ?>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 py-5">
+                <h5 class="font-baloo font-size-20"><?php echo $item['item_name'] ?? "Unknown"; ?></h5>
+                <small><?php echo $item['item_type'] ?? "Type"; ?></small>
+                <div class="d-flex">
+                    <div class="rating text-warning font-size-12">
+                        <span><i class="fas fa-star"></i></span>
+                        <span><i class="fas fa-star"></i></span>
+                        <span><i class="fas fa-star"></i></span>
+                        <span><i class="fas fa-star"></i></span>
+                        <span><i class="far fa-star"></i></span>
+                    </div>
+                    <a href="#" class="px-2 font-rale font-size-14">20,534 ratings | 1000+ answered questions</a>
+                </div>
+                <hr class="m-0">
+
+                <!---    product price       -->
+                <table class="my-3">
+                    <tr class="font-rale font-size-14">
+                        <td>M.R.P:</td>
+                        <td><strike>350 EGP</strike></td>
+                    </tr>
+                    <tr class="font-rale font-size-14">
+                        <td>Deal Price:</td>
+                        <td class="font-size-20 text-danger"><span> <span><?php echo $item['item_price'] ?? 0; ?></span> EGP<small class="text-dark font-size-12">&nbsp;&nbsp;Inclusive of all taxes</small></td>
+                    </tr>
+                    <tr class="font-rale font-size-14">
+                        <td>You Save:</td>
+                        <td><span class="font-size-16 text-danger">50 EGP</span></td>
+                    </tr>
+                </table>
+                <!---    !product price       -->
+
+                <!--    #policy -->
+                <div id="policy">
+                    <div class="d-flex">
+                        <div class="return text-center mr-5">
+                            <div class="font-size-20 my-2 color-second">
+                                <span class="fas fa-retweet border p-3 rounded-pill"></span>
+                            </div>
+                            <a href="#" class="font-rale font-size-12">10 Days <br> Replacement</a>
+                        </div>
+                        <div class="return text-center mr-5">
+                            <div class="font-size-20 my-2 color-second">
+                                <span class="fas fa-truck  border p-3 rounded-pill"></span>
+                            </div>
+                            <a href="#" class="font-rale font-size-12">Daily Tuition <br>Deliverd</a>
+                        </div>
+                        <div class="return text-center mr-5">
+                            <div class="font-size-20 my-2 color-second">
+                                <span class="fas fa-check-double border p-3 rounded-pill"></span>
+                            </div>
+                            <a href="#" class="font-rale font-size-12">1 Year <br>Warranty</a>
+                        </div>
+                    </div>
+                </div>
+                <!--    !policy -->
+                <hr>
+                <div class="row">
+                    <div class="col-6">
+                    </div>
+                </div>
+                <!-- order-details -->
+                <div id="order-details" class="font-rale d-flex flex-column text-dark">
+                    <small>Delivery by : Mar 29  - Apr 1</small>
+                    <small>Sold by <a href="#">Daily Electronics </a>(4.5 out of 5 | 18,198 ratings)</small>
+                    <small><i class="fas fa-map-marker-alt color-primary"></i>&nbsp;&nbsp;Deliver to Customer - 424201</small>
+                </div>
+                <!-- !order-details -->
+
+
+            </div>
+            <div class="col-12">
+                <h6 class="font-rubik">Product Description</h6>
+                <hr>
+                <p class="font-rale font-size-14"><?php echo $item['item_description'] ?? "Text"; ?></p>
+            </div>
+        </div>
+    </div>
+</section>
+<!--   !product  -->
+
+<?php
+    endif;
+    endforeach;
+?>
